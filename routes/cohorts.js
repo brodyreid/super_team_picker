d0dba285b160c65a63b8cc3f4ee58f71746b1df0
Brody Reid <brodyhreid@gmail.com>
2021-03-05 14:03:14 -0800

    added display all and one functionalities

--- routes/cohorts.js
+++ routes/cohorts.js
@@ -1,18 +1,23 @@
 const { response } = require('express')
+const { userParams } = require('../db/client')
 const knex = require('../db/client')
 const router = require('express').Router()
 
+// Homepage
 router.get('/', (request, response) => {
     response.render('home')
 })
 
+// Create new cohort
 router.get('/cohorts/new', (request, response) => {
   response.render('new')
 })
 
+// Display current cohorts
 router.get('/cohorts', (request, response) => {
-    const query = knex('cohorts')
+    return knex('cohorts')
       .orderBy('createdAt', 'DESC')
+      .select('*')
       .then(cohorts => {
         response.render('current', { cohorts })
     })
@@ -36,15 +41,16 @@ router.post('/cohorts', (request, response) => {
     })
 })
 
-// router.get('/confirmed', (request, response) => {
-  
-//   const { logo, name, members } = request.query
+// Show specific cohort
+router.get('/cohorts/:id', (request, response) => {
+  const id = request.params.id
 
-//   response.render('confirmed', {
-//     logo,
-//     name,
-//     members,
-//   })
-// })
+  return knex('cohorts')
+    .where('id', id)
+    .first()
+    .then(cohort => {
+      response.render('show', { cohort })
+    })
+})
   
 module.exports = router
